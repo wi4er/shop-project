992799e0ca1e1609dc7af5525b186cc5530c94a7
maximloboda <wo4er777@gmail.com>
2023-11-13 04:32:11 +0200

    cors middleware

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from "./user/user.module";
-import { ContentModule } from "./content/content.module";
-import { FileModule } from "./file/file.module";
-import { PropertyModule } from "./property/property.module";
-import { DirectoryModule } from "./directory/directory.module";
-import { FlagModule } from "./flag/flag.module";
-import { LangModule } from "./lang/lang.module";
-import { CommonModule } from "./common/common.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { createConnectionOptions } from "./createConnectionOptions";
+import { UserModule } from './user/user.module';
+import { ContentModule } from './content/content.module';
+import { FileModule } from './file/file.module';
+import { PropertyModule } from './property/property.module';
+import { DirectoryModule } from './directory/directory.module';
+import { FlagModule } from './flag/flag.module';
+import { LangModule } from './lang/lang.module';
+import { CommonModule } from './common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { createConnectionOptions } from './createConnectionOptions';
+import * as cors from 'cors';
 
 @Module({
   imports: [
@@ -27,4 +28,13 @@ import { createConnectionOptions } from "./createConnectionOptions";
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(cors({
+        credentials: true,
+        origin: true,
+      }))
+      .forRoutes('/');
+  }
+}
