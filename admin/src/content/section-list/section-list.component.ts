b5f8492a19d38a4fd106d50f39cd1aa0af89e2ca
maximloboda <wo4er777@gmail.com>
2025-04-24 12:45:36 +0300

    #86c355vnn Отображение в списке разделов контента активных флагов

--- admin/src/content/section-list/section-list.component.ts
+++ admin/src/content/section-list/section-list.component.ts
@@ -65,6 +65,7 @@ export class SectionListComponent implements OnChanges {
     return [
       'select',
       'action',
+      'flags',
       ...this.columns,
       'image',
     ];
@@ -96,6 +97,30 @@ export class SectionListComponent implements OnChanges {
     this.pageSize = event.pageSize;
 
     this.refreshData();
+  }
+
+  /**
+   *
+   */
+  getFlagsIcon(id: string) {
+    const list = this.activeFlags[id];
+    const icons: Array<{
+      icon: string | null,
+      title: string,
+      color: string | null,
+    }> = [];
+
+    for (const flag of this.flagList) {
+      if (list.includes(flag.id) && flag.icon) {
+        icons.push({
+          icon: flag.icon,
+          title: flag.id,
+          color: flag.color,
+        });
+      }
+    }
+
+    return icons;
   }
 
   /**
--- admin/src/edit/flag-value/flag-value.service.ts
+++ admin/src/edit/flag-value/flag-value.service.ts
@@ -7,7 +7,6 @@ export class FlagValueService {
 
   constructor() { }
 
-
   /**
    *
    */
